ab5d1d530621d5e667a96aec1d07d2a5ce51964b
jiggensnaggen <nickallee8529@gmail.com>
2019-03-06 09:32:42 -0600

    added frequency table files.
    this is a great step in the right direction!!!

---
+++ FrequencyTableProofOfWork/frequencyTable.py
@@ -0,0 +1,15 @@
+import xlwings as xw
+import pandas as pd
+
+
+@xw.func
+def hello(name):
+    return "hello {0}".format(name)
+
+@xw.func
+@xw.arg('x', pd.DataFrame, index=False, header=True) #takes in argument as dataframe
+@xw.ret(index=True, header=True, expand='table', numbers =int)#specifies how the data is returned
+def frequencyTable(x):
+    valueCounts = x.apply(pd.Series.value_counts)
+    return valueCounts
+
