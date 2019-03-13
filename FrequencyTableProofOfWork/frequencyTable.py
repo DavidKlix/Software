ae1ad56db0daabbed1014729f409b9bcdf92b7b8
DavidKlix <DKlix@github.com>
2019-03-13 09:02:13 -0500

    Trying to adjust stuff, didn't work

--- FrequencyTableProofOfWork/frequencyTable.py
+++ FrequencyTableProofOfWork/frequencyTable.py
@@ -12,11 +12,17 @@ def hello(name):
 def frequencyTable(x):
     valueCounts = x.apply(pd.Series.value_counts)
     return valueCounts
-'''
+#pd is the module
+#Series is a type
+#value_counts is a function
+@xw.func
+@xw.arg('x', pd.DataFrame, index=False, header=True) #takes in argument as dataframe
+@xw.ret(index=True, header=True, expand='table', numbers =int)#specifies how the data is returned
 def frequencyTablePercent(x):
-    valueCounts = x.apply(pd.Series.value_counts)
+    valueCounts = frequencyTable(x)
     total = 0
-    for n in Series:
+    for valueCounts in x:
         total += 1
-    return valueCounts/total
-'''
+    if total == 0:
+        return 0
+    return total
